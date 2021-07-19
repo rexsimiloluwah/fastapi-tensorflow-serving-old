7e4ac6ee7895a3cb15bf93cba3ce8c1dd475b3c9
rexsimiloluwah <adetoyosi2001@gmail.com>
2021-07-19 11:58:45 +0100

    Code formatting

--- app/main.py
+++ app/main.py
@@ -4,17 +4,18 @@ from helpers import read_img_file, predict
 
 app = FastAPI()
 
+
 @app.post("/predict", response_model=dict)
-async def predict_image(file : UploadFile = File(...)):
+async def predict_image(file: UploadFile = File(...)):
     extension = file.filename.split(".")[-1]
     if not extension in ["jpg", "png", "jpeg"]:
         raise HTTPException(
-            detail = {
-                "status" : False,
-                "error" : "Only image files are supported i.e .jpg, .jpeg, .png"
+            detail={
+                "status": False,
+                "error": "Only image files are supported i.e .jpg, .jpeg, .png"
             },
             status_code=422
-    )
+        )
 
     flower_img = read_img_file(await file.read())
     print(flower_img)
@@ -22,44 +23,9 @@ async def predict_image(file : UploadFile = File(...)):
 
     return {
         "predictions": list(predictions[0]),
-        "confidence": round(predictions[1],2),
+        "confidence": round(predictions[1], 2),
         "class": predictions[2]
     }
 
 if __name__ == "__main__":
     uvicorn.run(app, port=5000)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
